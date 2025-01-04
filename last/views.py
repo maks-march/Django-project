9621039ec9136bcaf3fc17086ef1f46a5a3d81b9
maks-march <98648053+maks-march@users.noreply.github.com>
2025-01-05 00:21:18 +0500

    api

--- last/views.py
+++ last/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+import requests
+from datetime import datetime, timedelta
+
+
 
 def last(request):
     template = 'last/last.html'
-    context = {}
+
+    BASE_DIR = 'https://api.hh.ru/vacancies'
+    date_from = (datetime.now() - timedelta(days=1)).isoformat()
+    profession = 'C++'
+    url = f"{BASE_DIR}?date_from={date_from}&text=${profession}&per_page=10&order_by=published_at";
+
+    headers = {
+        'Authorization': 'Bearer HH-User-Agent'  # Замените на ваш токен
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    vacancies = data.get('items', [])
+    context = {'vacancies': vacancies, 'response': response.status_code}
     return render(request, template, context)
