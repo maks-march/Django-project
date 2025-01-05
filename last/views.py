7088da90c0d9babf6aef875f3d898337008b8b00
maks-march <98648053+maks-march@users.noreply.github.com>
2025-01-05 21:08:44 +0500

    fixing

--- last/views.py
+++ last/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 import requests, locale
 from datetime import datetime, timedelta
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-
 def last(request):
     template = 'last/last.html'
     locale.setlocale(
